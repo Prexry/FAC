82ea1cc61c1995d8303e49eb5a4a6857a3bef24d
q;'fkqw,fq <88373778+Prexry@users.noreply.github.com>
2025-03-19 11:18:25 -0500

    Create policy_test.sh

---
+++ policy_test.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Please run this script as root. You can do so by using 'sudo su'."
+    exit
+fi
+
+
+
+sleep 1
+
+mkdir -p /tmp/overlay/etc/opt/chrome/policies/managed
+echo '{
+  "DeveloperToolsAvailability": 1,
+  "DefaultPopupsSetting": 1,
+  "AllowDeletingBrowserHistory": true,
+  "AllowDinosaurEasterEgg": true,
+  "IncognitoModeAvailability": 0,
+  "AllowScreenLock": true,
+  "ExtensionAllowedTypes": null,
+  "ExtensionInstallAllowlist": null,
+  "ExtensionInstallBlocklist": null,
+  "ExtensionSettings": null,
+  "PasswordManagerEnabled": "true",
+  "TaskManagerEndProcessEnabled": "true",
+  "UptimeLimit": "null",
+  "SystemTerminalSshAllowed": "true",
+  "SystemTimezone": "",
+  "IsolatedAppsDeveloperModeAllowed": "true",
+  "ForceGoogleSafeSearch": "false",
+  "ForceYouTubeRestrict": "0",
+  "EasyUnlockAllowed": "true",
+  "DisableSafeBrowsingProceedAnyway": "false",
+  "DeviceAllowNewUsers": "true",
+  "DevicePowerAdaptiveChargingEnabled": "true",
+  "DeviceGuestModeEnabled": "true",
+  "DeviceUnaffiliatedCrostiniAllowed": "true",
+  "VirtualMachinesAllowed": "true",
+  "CrostiniAllowed": "true",
+  "DefaultCookiesSetting": "1",
+  "VmManagementCliAllowed": "true",
+  "WifiSyncAndroidAllowed": "true",
+  "DeveloperToolsDisabled": "false",
+  "DeveloperToolsAvailability": "1",
+  "DeviceBlockDevmode": "false",
+  "UserBorealisAllowed": "true",
+  "InstantTetheringAllowed": "true",
+  "NearbyShareAllowed": "true",
+  "PinnedLauncherApps": "null",
+  "PrintingEnabled": "true",
+  "SmartLockSigninAllowed": "true",
+  "PhoneHubAllowed": "true",
+  "LacrosAvailability": "user_choice",
+  "WallpaperImage": null,
+  "ArcPolicy": {
+    "playStoreMode": "ENABLED",
+    "installType": "FORCE_INSTALLED",
+    "playEmmApiInstallDisabled": false,
+    "dpsInteractionsDisabled": false
+  },
+  "DeviceUserAllowlist": ""
+}' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
+cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
+mount --bind /tmp/overlay/etc /etc
+
+echo ""
+echo "Applied!"

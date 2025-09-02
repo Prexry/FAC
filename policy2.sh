96c7211d554cac1f196bdfcf6424c346068e8fb0
q;'fkqw,fq <88373778+Prexry@users.noreply.github.com>
2025-09-02 02:02:30 -0500

    Update policy2.sh

--- policy2.sh
+++ policy2.sh
@@ -74,7 +74,7 @@ echo '{
   "NetworkThrottlingEnabled": "false",
   "NetworkPredictionOptions": "0",
   "AllowedDomainsForApps": "",
-  "LidCloseAction": "0",
+  "LidCloseAction": "null",
   "DeviceUserAllowlist": ""
 }' > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
 cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
